43509aabf10792f31a9631752d0e5c74828cbfcb
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 12:55:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
      }
 
 	 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-	 puts programmer_hash[:dennis_ritchie][:languages]
+	 programmer_hash[:dennis_ritchie][:languages]
 end
