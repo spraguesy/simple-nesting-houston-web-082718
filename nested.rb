3e209ef9539cd1e7fbe310bd3066a6167f2be9b0
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 12:39:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash =
@@ -110,7 +111,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 
 
 end
 
