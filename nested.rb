692959a6a1af52de66abf3dab07e40557fc9ff09
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 12:42:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -99,9 +99,9 @@ def changing_alan
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+        alan_kay = {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          languages = ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
